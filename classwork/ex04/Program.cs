5a5f7b7cdee71b3c2407706815b5c2886f75e2fb
Sharaborin Evgeny <art-sound45@yandex.ru>
2022-05-17 08:13:40 +0500

    add example 04

---
+++ classwork/ex04/Program.cs
@@ -0,0 +1,11 @@
+﻿// Написать программу вычисления произведения чисел от 1 до N
+Console.Write("Введите число: ");
+int n = int.Parse(Console.ReadLine());
+int digit = 1;
+int temp = 1;
+while (digit <= n)
+{
+	temp = temp * digit;
+	digit++;
+}
+Console.WriteLine($"Произведение чисел от 1 до {n} = {temp}");
