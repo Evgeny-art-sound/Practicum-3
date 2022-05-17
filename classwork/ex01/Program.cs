34f9a833e4b45aa49605ccb73eccecf92d7a393c
Sharaborin Evgeny <art-sound45@yandex.ru>
2022-05-17 07:25:35 +0500

    add 2 example

---
+++ classwork/ex01/Program.cs
@@ -0,0 +1,17 @@
+﻿// Показать таблицу квадратов чисел от 1 до N
+Console.Write("Введите число ");
+int n = int.Parse(Console.ReadLine());
+int curent_digit = 1;
+while (curent_digit <= n)
+{
+	Console.Write($"квадрат {curent_digit} равен  {Math.Pow(curent_digit, 2)} , ");
+	curent_digit++;
+}
+
+// // Показать таблицу квадратов чисел от 1 до N
+// Console.WriteLine("Введите число n: ");
+// int n = int.Parse(Console.ReadLine());
+// for (int i = 1; i <=n; i++)
+// {
+// Console.Write($"{i * i} ");
+// }
---
+++ classwork/ex02/Program.cs
@@ -0,0 +1,12 @@
+﻿// Найти сумму чисел от 1 до А
+Console.Write("Введите число A ");
+int a = int.Parse(Console.ReadLine());
+int curent_digit = 1;
+int curent_sum = 0;
+while (curent_digit <= a)
+{
+	curent_sum = curent_digit+curent_sum;
+	curent_digit++;
+
+}
+Console.WriteLine($"сумма всех чисел от 1 до А  = {curent_sum}");
