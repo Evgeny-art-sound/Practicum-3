00c2bb18c7f1eafeb603fc985929d240be1c927f
Sharaborin Evgeny <art-sound45@yandex.ru>
2022-05-17 07:40:38 +0500

    add example 03

---
+++ classwork/ex03/Program.cs
@@ -0,0 +1,10 @@
+﻿// Определить количество цифр в числе
+Console.Write("Введите число ");
+int a = int.Parse(Console.ReadLine());
+int digit = 0;
+while (a>0)
+{
+a = a/10;
+digit ++;
+}
+Console.WriteLine($"Количество цифр в числе: {digit}");
