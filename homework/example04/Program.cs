1ff8b071544f02f38d8a3b692f2e32954cb89b72
Sharaborin Evgeny <art-sound45@yandex.ru>
2022-05-23 00:51:47 +0500

    add example 4

---
+++ homework/example04/Program.cs
@@ -0,0 +1,11 @@
+﻿// Возведите число А в натуральную степень B используя цикл
+Console.Write("Введите число: ");
+int a = int.Parse(Console.ReadLine());
+Console.Write("Введите степень, в которую нужно возвести число: ");
+int b = int.Parse(Console.ReadLine());
+int result = a;
+for (int i = 1; i < b; i++)
+{
+	result = result * a;
+}
+Console.WriteLine($"Число {a} в степени {b} = {result} ");
