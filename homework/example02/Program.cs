0d1a11b3e5c566258eb775fd9257bf0c7981f6cc
Sharaborin Evgeny <art-sound45@yandex.ru>
2022-05-22 23:50:41 +0500

    add example 2

---
+++ homework/example02/Program.cs
@@ -0,0 +1,9 @@
+﻿// Найти кубы чисел от 1 до N
+Console.WriteLine("Введите число: ");
+int n = int.Parse(Console.ReadLine());
+int digit = 0;
+for (int a = 1; a <= n; a++)
+{
+	Console.WriteLine($"куб числа {a} {Math.Pow(a,3)}");;
+}
+
