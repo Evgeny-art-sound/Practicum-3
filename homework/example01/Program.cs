0847a5d6317a09d644c063d22326f9607341048f
Sharaborin Evgeny <art-sound45@yandex.ru>
2022-05-22 23:03:11 +0500

    add 1 example homework

---
+++ homework/example01/Program.cs
@@ -0,0 +1,9 @@
+﻿// Показать таблицу квадратов чисел от 1 до N
+Console.WriteLine("Введите число N");
+int n = int.Parse(Console.ReadLine());
+int digit = 0;
+for (int a = 1; a <= n; a++)
+{
+	digit = a * a;
+	Console.WriteLine($"Квадрат числа {a} = {digit}");
+}
