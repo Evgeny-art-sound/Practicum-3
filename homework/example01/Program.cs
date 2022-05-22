ca8c9da2d9fd9f5eedb5e781e74fc395932cf706
Sharaborin Evgeny <art-sound45@yandex.ru>
2022-05-23 00:21:33 +0500

    add example 3, bag fix

--- homework/example01/Program.cs
+++ homework/example01/Program.cs
@@ -1,5 +1,5 @@
 ﻿// Показать таблицу квадратов чисел от 1 до N
-Console.WriteLine("Введите число N");
+Console.Write("Введите число ");
 int n = int.Parse(Console.ReadLine());
 int digit = 0;
 for (int a = 1; a <= n; a++)
---
+++ homework/example03/Program.cs
@@ -0,0 +1,9 @@
+﻿// Найти сумму чисел от 1 до А
+Console.Write("Введите число N ");
+int n = int.Parse(Console.ReadLine());
+int digit = 0;
+for (int a = 0; a <= n; a++)
+{
+	digit = digit + a;
+}
+Console.WriteLine($"Сумма чисел от 1 до N = {digit}");
