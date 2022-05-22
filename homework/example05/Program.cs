53d0e828e905da61c9d1b6b1f946a4495b4606ce
Sharaborin Evgeny <art-sound45@yandex.ru>
2022-05-23 02:11:44 +0500

    add ex 6

---
+++ homework/example05/Program.cs
@@ -0,0 +1,11 @@
+﻿// Подсчитать сумму цифр в числе
+Random rnd = new Random();
+int x = rnd.Next(0,100000);
+Console.WriteLine($" рандомное число = {x}");
+int sum = 0;
+while(x > 0)
+{
+    sum = sum + x % 10;
+    x = x / 10;
+}
+Console.WriteLine($" сумма чисел в рандомном числе = {sum} ");
