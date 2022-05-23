873ff27edc66adc1cf25ac52f453c909168483fc
Sharaborin Evgeny <art-sound45@yandex.ru>
2022-05-23 10:46:37 +0500

    add eexample 6 & 7

--- homework/example06/Program.cs
+++ homework/example06/Program.cs
@@ -1,1 +1,19 @@
 ﻿// Показать кубы чисел, заканчивающихся на четную цифру
+Console.WriteLine("Показать кубы чисел, заканчивающихся на четную цифру в рандомном массиве ");
+int[] array = new int[5];
+Random rand = new Random();
+Console.WriteLine($"Рандомный массив состит из: ");
+for (int a = 0; a < 5; a++)
+{
+	array[a] = rand.Next(-1000, 1000);
+	Console.Write($"{array[a]} ");
+	if (array[a] % 2 == 0)
+	{
+		Console.WriteLine($"Число четное, его куб =  {Math.Pow(array[a], 3)}");
+	}
+	else if (array[a] % 2 != 0)
+		{
+			Console.WriteLine("Число не четное ");
+		}
+	
+}
---
+++ homework/example07/Program.cs
@@ -0,0 +1,9 @@
+﻿// Задать массив из 8 элементов, заполненных нулями и единицами вывести их на экран
+int[] array = new int[8];
+Random rand = new Random();
+Console.WriteLine($"Рандомный массив состит из 8 элементов содержит: ");
+for (int a = 0; a < 8; a++)
+{
+	array[a] = rand.Next(0,2);
+	Console.Write($"{array[a]} ");
+}
